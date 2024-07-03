a583f18fe778088cc43973beea0a01ebb5c0d441
Lenício Jr <lenicio.junior@hotmail.com>
2024-07-03 10:49:56 -0300

    Atualizar Prato

--- src/main/java/br/com/porz/resprozante/controller/RestauranteController.java
+++ src/main/java/br/com/porz/resprozante/controller/RestauranteController.java
@@ -4,7 +4,10 @@ import br.com.porz.resprozante.dto.PratoDto;
 import br.com.porz.resprozante.dto.RestauranteDto;
 import br.com.porz.resprozante.model.Prato;
 import br.com.porz.resprozante.model.Restaurante;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurante")
@@ -22,16 +25,44 @@ public class RestauranteController {
 
   @PostMapping("/prato")
   public Prato adicionarPrato(@RequestBody PratoDto pratoDto) {
-     return restaurante.adicionar(new Prato(
+    return restaurante.adicionar(new Prato(
         restaurante.getIdPrato(),
         pratoDto.nome(),
         pratoDto.valor(),
         pratoDto.tipoPrato()
     ));
-
   }
 
 
+  @GetMapping("/pratos")
+  public List<Prato> listaPratos() {
+    return restaurante.getPratos();
+  }
+
+
+  @GetMapping("/prato")
+  public ResponseEntity<Prato> buscarPrato(@RequestParam long id) {
+    for (Prato prato : restaurante.getPratos()) {
+      if (prato.getId() == id) {
+        return ResponseEntity.ok(prato);
+      }
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+
+  @PutMapping("/prato")
+  public ResponseEntity<Prato> editaPrato(@RequestParam long id, @RequestBody PratoDto pratoDto) {
+    for (Prato prato : restaurante.getPratos()) {
+      if (prato.getId() == id) {
+        prato.setNome(pratoDto.nome());
+        prato.setValor(pratoDto.valor());
+        prato.setTipoPrato(pratoDto.tipoPrato());
+        return ResponseEntity.ok(prato);
+      }
+    }
+    return ResponseEntity.notFound().build();
+  }
 
 
 }
